3624be2d3a521d60c11fd3ce36774abad76e0c46
YeeJingGan <152396552+YeeJingGan@users.noreply.github.com>
2024-12-16 12:44:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cdc8ewmqsqil.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "12345678"
+customdb = "employee"
+custombucket = "s3-ganyeejing-bucket"
+customregion = "us-east-1"
+
